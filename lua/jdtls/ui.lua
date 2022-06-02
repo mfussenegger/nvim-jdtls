6af471afc735845060ed69bdee165cca6ca07677
Abhishek Banerji <40197742+Abh15h3k@users.noreply.github.com>
2022-06-02 14:10:59 +0530

    Change pick_many to prompt for all choices at once (#272)

--- lua/jdtls/ui.lua
+++ lua/jdtls/ui.lua
@@ -30,15 +30,29 @@ function M.pick_many(items, prompt, label_f)
   if not items or #items == 0 then
     return {}
   end
+
+  label_f = label_f or function(item)
+    return item
+  end
+
+  local choices = {}
+  for i, item in pairs(items) do
+    table.insert(choices, string.format("%d. %s", i, label_f(item)))
+  end
+
   local selected = {}
-  for _, item in pairs(items) do
-    local choice = vim.fn.inputlist({
-      string.format('\n%s %s', prompt, label_f(item)),
-      "1. Yes",
-      "2. No"
-    })
-    if choice == 1 then
-      table.insert(selected, item)
+  while true do
+    local answer = vim.fn.input(string.format("\n%s\n%s (Esc to finish): ", table.concat(choices, "\n"), prompt))
+    if answer == "" then
+      break
+    end
+
+    local index = tonumber(answer)
+    if index ~= nil then
+      if string.find(choices[index], "*") == nil then
+        table.insert(selected, items[index])
+        choices[index] = choices[index] .. " *"
+      end
     end
   end
   return selected
