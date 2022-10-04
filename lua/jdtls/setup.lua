mfussenegger/nvim-jdtls

commit f3b916d6ee7bf526da78a628270a205f2ea9c32c

Extend user provided capabilites with default capabilities (#324)
Allows users to only provide a subset of the default capabilities.

Co-authored-by: lidulibai <zq@zeqiandeMacBook-Pro.local>

lua/jdtls/setup.lua
@@ -236,7 +236,7 @@ function M.start_or_attach(config)
   config.handlers['language/progressReport'] = config.handlers['language/progressReport'] or progress_report
   config.handlers['language/status'] = config.handlers['language/status'] or status_callback
   config.handlers['workspace/configuration'] = config.handlers['workspace/configuration'] or configuration_handler
-  local capabilities = config.capabilities or lsp.protocol.make_client_capabilities()
+  local capabilities = vim.tbl_deep_extend('keep', config.capabilities, lsp.protocol.make_client_capabilities())
   local extra_capabilities = {
     textDocument = {
       codeAction = {

commit c52c136b3b9c982d56bd9929750b7738571da083

Fix nil error when capabilities are not provided in config (#339)
capabilities is optional
Fixes regression introduced with https://github.com/mfussenegger/nvim-jdtls/pull/324

Co-authored-by: lidulibai <zq@zeqiandeMacBook-Pro.local>

lua/jdtls/setup.lua
@@ -236,7 +236,7 @@ function M.start_or_attach(config)
   config.handlers['language/progressReport'] = config.handlers['language/progressReport'] or progress_report
   config.handlers['language/status'] = config.handlers['language/status'] or status_callback
   config.handlers['workspace/configuration'] = config.handlers['workspace/configuration'] or configuration_handler
-  local capabilities = vim.tbl_deep_extend('keep', config.capabilities, lsp.protocol.make_client_capabilities())
+  local capabilities = vim.tbl_deep_extend('keep', config.capabilities or {}, lsp.protocol.make_client_capabilities())
   local extra_capabilities = {
     textDocument = {
       codeAction = {