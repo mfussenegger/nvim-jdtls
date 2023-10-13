a09f8cdd75c546eb024d0f0b5b3ad185f05b738f
Mathias Fussenegger <f.mathias@zignar.net>
2023-10-13 17:58:19 +0200

    Add executeClientCommandSupport to extendedClientCapabilities
    To account for upstream change in https://github.com/eclipse-jdtls/eclipse.jdt.ls/pull/2904

--- lua/jdtls/setup.lua
+++ lua/jdtls/setup.lua
@@ -93,22 +93,23 @@ end
 
 
 M.extendedClientCapabilities = {
-  classFileContentsSupport = true;
-  generateToStringPromptSupport = true;
-  hashCodeEqualsPromptSupport = true;
-  advancedExtractRefactoringSupport = true;
-  advancedOrganizeImportsSupport = true;
-  generateConstructorsPromptSupport = true;
-  generateDelegateMethodsPromptSupport = true;
-  moveRefactoringSupport = true;
-  overrideMethodsPromptSupport = true;
+  classFileContentsSupport = true,
+  generateToStringPromptSupport = true,
+  hashCodeEqualsPromptSupport = true,
+  advancedExtractRefactoringSupport = true,
+  advancedOrganizeImportsSupport = true,
+  generateConstructorsPromptSupport = true,
+  generateDelegateMethodsPromptSupport = true,
+  moveRefactoringSupport = true,
+  overrideMethodsPromptSupport = true,
+  executeClientCommandSupport = true,
   inferSelectionSupport = {
     "extractMethod",
     "extractVariable",
     "extractConstant",
     "extractVariableAllOccurrence"
-  };
-};
+  },
+}
 
 
 local function configuration_handler(err, result, ctx, config)
