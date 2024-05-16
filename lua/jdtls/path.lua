29255ea26dfb51ef0213f7572bff410f1afb002d
Mathias Fussenegger <f.mathias@zignar.net>
2024-05-16 16:48:42 +0200

    Fix nvim-0.11 deprecation warnings

--- lua/jdtls/path.lua
+++ lua/jdtls/path.lua
@@ -5,6 +5,9 @@ local is_windows = vim.loop.os_uname().version:match('Windows')
 M.sep = is_windows and '\\' or '/'
 
 function M.join(...)
+  if vim.fs.joinpath then
+    return vim.fs.joinpath(...)
+  end
   local result = table.concat(vim.tbl_flatten {...}, M.sep):gsub(M.sep .. '+', M.sep)
   return result
 end
